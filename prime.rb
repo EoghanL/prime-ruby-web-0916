53231bcd0e8207b5a3cdf6aaa0bf370c5ee46103
Eoghan Leddy <eoghan.leddy@flatironschool.com>
2016-09-23 18:58:46 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(int)
+  return false if int <= 1
+  return true if int == 2
+  counter = 2
+  until(counter ** 2 > int + 3)
+    if(int % counter == 0)
+      return false
+    else
+      counter += 1
+    end
+  end
+  return true
+end
